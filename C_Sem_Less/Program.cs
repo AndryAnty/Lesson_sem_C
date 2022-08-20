37951c842fadbdd5284d2b552360d205f6d6eb00
[Андрей Антонов] <[andrey.antonov92@gmail.com]>
2022-08-20 18:58:44 +0300

    Задача 2. Нахождение максимального числа их двух чисел

---
+++ C_Sem_Less/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+{
+    Console.WriteLine("Самое большое число " + a);
+}
+else
+{
+    Console.WriteLine("Самое больше число " + b);
+}
+
