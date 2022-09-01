8d41f6ff2e4bc461ea128127ab9f0315536c0fd8
[Андрей Антонов] <[andrey.antonov92@gmail.com]>
2022-09-01 15:15:17 +0300

    ДЗ к 3 мминару

---
+++ 3_Less_ssem/HomeWork_3/task_21/Program.cs
@@ -0,0 +1,22 @@
+﻿// Решние не моё
+
+double Distance(double xa, double ya, double za, double xb, double yb, double zb )
+{
+	double result = Math.Sqrt(Math.Pow((xa - xb), 2) + Math.Pow((ya - yb), 2) + Math.Pow((za - zb), 2));
+    // 
+	return Math.Round(result, 2);
+}
+
+
+
+
+
+Console.WriteLine("Введите координаты точки А: ");
+double xa = Convert.ToDouble(Console.ReadLine());
+double ya = Convert.ToDouble(Console.ReadLine());
+double za = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B: ");
+double xb = Convert.ToDouble(Console.ReadLine());
+double yb = Convert.ToDouble(Console.ReadLine());
+double zb = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(Distance(xa, ya, za, xb, yb, zb));
---
+++ 3_Less_ssem/task_19/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+
+void PalNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine("Это палиндром");
+  }
+  else Console.WriteLine("Нe палиндром");
+}
+
+if (number.Length == 5){
+  PalNumber(number);
+}
+else 
+Console.WriteLine($"Введи правильное число");
