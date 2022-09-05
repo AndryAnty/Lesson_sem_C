ee62ddcc314a4225ec988a4259945d374709d2cf
[Андрей Антонов] <[andrey.antonov92@gmail.com]>
2022-09-05 14:18:59 +0300

    ДЗ к 4 семинару

---
+++ 3_Less_ssem/seminar_4/task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+Console.WriteLine("введите a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите b");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Function(a, b);
+
+void Function(int a, int b) 
+{
+    int count;
+    for(count = 1; count < b; count++);
+    {
+        int result = Convert.ToInt32(Math.Pow(a, b));
+        Console.WriteLine(result);
+    }
+}
---
+++ 3_Less_ssem/seminar_4/task_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] arr = new int[7];
+Random rand = new Random();
+for(int i = 0; i < arr.Length; i++)
+arr[i] = rand.Next();
+
+PrintArray(arr);
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    for(int j = 0; j < count; j++)
+    {
+        Console.Write($"{arr[j]} ");
+    }
+    Console.WriteLine();
+}
+
