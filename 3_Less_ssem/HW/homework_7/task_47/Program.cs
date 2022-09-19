bd145848bbc8986d4ac42ed7571e18695261f23b
Andry <andrey.antonov92@gmail.com>
2022-09-19 13:22:57 +0300

    Задача 47

---
+++ 3_Less_ssem/HW/homework_7/task_47/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+
+Console.WriteLine("Введие количество строк");
+int rows = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+
+PrintArray(array);
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.WriteLine($"{inArray[i, j]}");
+        }
+    }
+    Console.WriteLine();
+}
